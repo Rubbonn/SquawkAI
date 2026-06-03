35edb1e19d67927c2eb2adaa708125d82b04d88f
Ruben Giuriato <r.giuriato@hostinato.it>
2026-06-03 09:21:48 +0200

    Aggiunta finestra di dev tools del browser per lo sviluppo.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv(os.path.join(base_dir, '.env'))
 node_env: str = os.environ.get('NODE_ENV', 'production')
 
 if __name__ == "__main__":
-	from PySide6.QtCore import QUrl
+	from PySide6.QtCore import QUrl, Qt
 	from PySide6.QtWidgets import QApplication
 	from PySide6.QtWebEngineWidgets import QWebEngineView
 	from PySide6.QtWebChannel import QWebChannel
@@ -19,10 +19,19 @@ if __name__ == "__main__":
 	app = QApplication([])
 
 	browser = QWebEngineView()
+	browser.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose)
 	target = QUrl.fromLocalFile(os.path.join(base_dir, 'app/frontend/dist/index.html')) if node_env == 'production' else QUrl('http://localhost:5173')
 	browser.load(target)
 	browser.resize(1280, 1024)
 	browser.show()
+
+	if node_env == 'development':
+		dev_tools = QWebEngineView()
+		dev_tools.setWindowFlag(Qt.WindowType.WindowCloseButtonHint, False)
+		browser.destroyed.connect(dev_tools.close)
+		browser.page().setDevToolsPage(dev_tools.page())
+		dev_tools.resize(1280, 512)
+		dev_tools.show()
 
 	channel = QWebChannel()
 	from app.utils.bridge import Bridge
