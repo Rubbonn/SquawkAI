391328b30bd53a65b358832bfce88fca0fdda7e6
Ruben Giuriato <gujojo@hotmail.it>
2026-06-09 17:15:52 +0200

    Spostata finestra principale con gestione dei web tools.

---
+++ app/windows.py
@@ -0,0 +1,26 @@
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QCloseEvent
+from PySide6.QtWebEngineWidgets import QWebEngineView
+
+class MainWindow(QWebEngineView):
+	def __init__(self, debug: bool = False):
+		super().__init__()
+		self.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose)
+		self._web_tools = None
+
+		if debug:
+			self._web_tools = QWebEngineView()
+			self._web_tools.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose)
+			self._web_tools.setWindowFlag(Qt.WindowType.WindowCloseButtonHint, False)
+			self._web_tools.resize(1280, 512)
+			self.page().setDevToolsPage(self._web_tools.page())
+	
+	def closeEvent(self, event: QCloseEvent) -> None:
+		if self._web_tools is not None:
+			self._web_tools.close()
+		return super().closeEvent(event)
+	
+	def show(self) -> None:
+		super().show()
+		if self._web_tools is not None:
+			self._web_tools.show()
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ dotenv.load_dotenv(os.path.join(base_dir, '.env'))
 node_env: str = os.environ.get('NODE_ENV', 'production')
 
 if __name__ == "__main__":
-	from PySide6.QtCore import QUrl, Qt
+	from app.windows import MainWindow
+	from PySide6.QtCore import QUrl
 	from PySide6.QtWidgets import QApplication
-	from PySide6.QtWebEngineWidgets import QWebEngineView
 	from PySide6.QtWebChannel import QWebChannel
 
 	QApplication.setOrganizationName('Ruben Giuriato')
@@ -18,20 +18,11 @@ if __name__ == "__main__":
 
 	app = QApplication([])
 
-	browser = QWebEngineView()
-	browser.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose)
+	browser = MainWindow(debug=(node_env == 'development'))
 	target = QUrl.fromLocalFile(os.path.join(base_dir, 'app/frontend/dist/index.html')) if node_env == 'production' else QUrl('http://localhost:5173')
 	browser.load(target)
 	browser.resize(1280, 1024)
 	browser.show()
-
-	if node_env == 'development':
-		dev_tools = QWebEngineView()
-		dev_tools.setWindowFlag(Qt.WindowType.WindowCloseButtonHint, False)
-		browser.destroyed.connect(dev_tools.close)
-		browser.page().setDevToolsPage(dev_tools.page())
-		dev_tools.resize(1280, 512)
-		dev_tools.show()
 
 	channel = QWebChannel()
 	from app.utils.bridge import bridge
